b17de1287601dfa2486d6879c20156ab34ea8568
Vladas Pipiras <vladaspipiras@Vladass-iMac.fios-router.home>
2018-03-19 11:40:09 -0400

    Added AR(2) model for particles

--- R/LGC.R
+++ R/LGC.R
@@ -49,14 +49,19 @@ LGC <- function(x, count.family = c("Poisson", "mixed-Poisson"),
   if(gauss.series=="AR"){
     if(is.null(p)) stop("you must specify the AR order, p, to use
                         gauss.series=AR")
-    if(p>1) stop("the ACVF is not coded for AR models of order higher than 1
+    if(p>2) stop("the ACVF is not coded for AR models of order higher than 1
                  currently")
     if(p==1){
       gamZ = function(h, phi){ phi^h / (1-phi^2) }
       gauss.initial = function(x){ acf(x, plot = FALSE)$acf[2] }
       n.theta1.idx = theta1.idx[length(theta1.idx)] # num params in theta1
       theta2.idx = (n.theta1.idx + 1):(n.theta1.idx + 1)
-    }else{ stop("the p specified is not valid") }
+    }
+    if(p==2){
+      n.theta1.idx = theta1.idx[length(theta1.idx)] # num params in theta1
+      theta2.idx = (n.theta1.idx + 1):(n.theta1.idx + 2)
+    }
+    # else{ stop("the p specified is not valid") }
   }
 
   # ----------------------------------------------------------------------------
@@ -96,7 +101,7 @@ LGC <- function(x, count.family = c("Poisson", "mixed-Poisson"),
   if ((gauss.series=="AR") & (estim.method=="particlesSIS")){
     if(is.null(p)) stop("you must specify the AR order, p, to use
                         gauss.series=AR")
-    if(p>1) stop("the ACVF is not coded for AR models of order higher than 1
+    if(p>2) stop("the ACVF is not coded for AR models of order higher than 1
                  currently")
     if(p==1){
       #set.seed(1)
@@ -105,7 +110,7 @@ LGC <- function(x, count.family = c("Poisson", "mixed-Poisson"),
         qnorm(runif(length(a),0,1)*(pnorm(b,0,1)-pnorm(a,0,1))+pnorm(a,0,1),0,1)
       }
       likSIS = function(theta, data){
-        set.seed(1)
+        #set.seed(1)
         theta1 = theta[theta1.idx]
         n.theta1.idx = theta1.idx[length(theta1.idx)] # num params in theta1
         theta2.idx = (n.theta1.idx + 1):(n.theta1.idx + 1)
@@ -148,7 +153,81 @@ LGC <- function(x, count.family = c("Poisson", "mixed-Poisson"),
         out = if (is.na(nloglik)) Inf else nloglik
         return(out)
       }
-    }else{ stop("the p specified is not valid") }
+    }
+    if(p==2){
+      #set.seed(1)
+      z.rest = function(a,b){
+        # Generates N(0,1) variables restricted to (ai,bi),i=1,...n
+        qnorm(runif(length(a),0,1)*(pnorm(b,0,1)-pnorm(a,0,1))+pnorm(a,0,1),0,1)
+      }
+      likSIS = function(theta, data){
+        # set.seed(1)
+        theta1 = theta[theta1.idx]
+        n.theta1.idx = theta1.idx[length(theta1.idx)] # num params in theta1
+        theta2.idx = (n.theta1.idx + 1):(n.theta1.idx + 2)
+        phi = theta[theta2.idx]
+
+        if ( (phi[2]+phi[1]<1) & (phi[2]-phi[1]<1) & (abs(phi[2])<1)){
+          xt = data
+          T1 = length(xt)
+          N = 1000 # number of particles
+          prt = matrix(0,N,T1) # to collect all particles
+          wgh = matrix(0,N,T1) # to collect all particle weights
+
+          a1 = qnorm(cdf(xt[1]-1,theta1),0,1)
+          b1 = qnorm(cdf(xt[1],theta1),0,1)
+          a1 = rep(a1,N)
+          b1 = rep(b1,N)
+          zprev1 = z.rest(a1,b1)
+          zhat1 = phi[1]*zprev1
+          prt[,1] = zhat1
+
+          wprev = rep(1,N)
+          wgh[,1] = wprev
+
+          phi0 = if(abs(phi[1])<0.9){phi[1]}else{0.9*sign(phi[1])}
+          rt2 = sqrt(1-phi0^2)
+          a2 = (qnorm(cdf(xt[2]-1,theta1),0,1) - phi[1]*zprev1)/rt2
+          b2 = (qnorm(cdf(xt[2],theta1),0,1) - phi[1]*zprev1)/rt2
+          err2 = z.rest(a2,b2)
+          znew2 = phi0*zprev1 + rt2*err2
+          znew = c(znew2,zprev1)
+          zhat2 = sum(phi*znew)
+          prt[,2] = zhat2
+
+          wgh[,2] = wprev*(pnorm(b2,0,1) - pnorm(a2,0,1))
+          wprev = wgh[,2]
+
+          zprev = znew
+
+          for (t in 3:T1)
+          {
+            rt = 1/sqrt( ((1-phi[2])/(1+phi[2])) / ((1-phi[2])^2-phi[1]^2) )
+            a = (qnorm(cdf(xt[t]-1,theta1),0,1) - sum(phi*zprev))/rt
+            b = (qnorm(cdf(xt[t],theta1),0,1) - sum(phi*zprev))/rt
+            err = z.rest(a,b)
+            znew = sum(phi*zprev) + rt*err
+            znew = c(znew,zprev[1])
+            zhat = sum(phi*znew)
+            prt[,t] = zhat
+            zprev = znew
+
+            wgh[,t] = wprev*(pnorm(b,0,1) - pnorm(a,0,1))
+            wprev = wgh[,t]
+          }
+
+          lik = pdf(xt[1],theta1)*mean(wgh[,T1])
+          nloglik = (-2)*log(lik)
+
+          out = if (is.na(nloglik)) Inf else nloglik
+          return(out)
+        }else{
+          out = Inf
+          return(out)
+        }
+      }
+    }
+    else{ stop("the p specified is not valid") }
   }
 
   if(estim.method=="gaussianLik"){
@@ -158,8 +237,14 @@ LGC <- function(x, count.family = c("Poisson", "mixed-Poisson"),
   }
 
   if((gauss.series=="AR") & (estim.method=="particlesSIS")){
-    R0 <- DEoptim::DEoptim(likSIS, lower = c(theta1.min,-.99), upper = c(theta1.max,.99),control = DEoptim::DEoptim.control(trace = 10, itermax = 100, steptol = 50, reltol = 1e-5), data=x)
-    optim.output <- as.vector(R0$optim$bestmem)
+    if(p==1){
+      R0 <- DEoptim::DEoptim(likSIS, lower = c(theta1.min,-.99), upper = c(theta1.max,.99),control = DEoptim::DEoptim.control(trace = 10, itermax = 100, steptol = 50, reltol = 1e-5), data=x)
+      optim.output <- as.vector(R0$optim$bestmem)
+    }
+    if(p==2){
+      R0 <- DEoptim::DEoptim(likSIS, lower = c(theta1.min,-1.99,-.99), upper = c(theta1.max,1.99,.99),control = DEoptim::DEoptim.control(trace = 10, itermax = 100, steptol = 50, reltol = 1e-5), data=x)
+      optim.output <- as.vector(R0$optim$bestmem)
+    }
   }
 
 
--- tests/simulate_poission_ar1.R
+++ tests/simulate_poission_ar1.R
@@ -7,14 +7,38 @@ sim_pois_ar1 = function(n, phi, lam){
   return(x)
 }
 
+#set.seed(Sys.time())
 x=sim_pois_ar1(200,0.7,2)
+plot.ts(x)
+acf(x,lag.max = 20)
 
-LGC(x, count.family = "poisson",
+LGC(x, count.family = "Poisson",
        gauss.series = "AR", p=1,
        estim.method = "gaussianLik")
 
-LGC(x, count.family = "poisson",
+LGC(x, count.family = "Poisson",
        gauss.series = "AR", p=1,
        estim.method = "particlesSIS")
 
+
+# Brockwell and Davis UG book, pp. 90-93
+xi1 = 2
+xi2 = 5
+
+xi1 = 2*(1+1i*sqrt(3))/3
+xi2 = 2*(1-1i*sqrt(3))/3
+
+phi1 = Re(1/xi1 + 1/xi2)
+phi2 = -Re((1/xi1) * (1/xi2))
+
+c(phi1,phi2)
+
+#set.seed(Sys.time())
+x=sim_pois_ar1(200,c(phi1,phi2),2)
+plot.ts(x)
+acf(x,lag.max = 20)
+
+LGC(x, count.family = "Poisson",
+    gauss.series = "AR", p=2,
+    estim.method = "particlesSIS")
 
