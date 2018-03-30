31b6ef2f51fd939dc56aaecc09308993fbb06658
Jim Livsey <james.a.livsey@gmail.com>
2018-03-30 14:19:01 -0400

    added parallel code to poisson-ar1 sim

--- tests/simulations/sim_poisson_ar1.R
+++ tests/simulations/sim_poisson_ar1.R
@@ -30,3 +30,42 @@ boxplot(param.est[,1], main="lambda (true value 2)"); abline(h=2, lty="dotted", 
 boxplot(param.est[,2], main="phi (true value 0.2)"); abline(h=0.2, lty="dotted", col=2)
 par(op)
 }
+
+
+# ---- Windows parallel -------------------------------------------------------
+library(parallel)
+# load data as list
+l2 <- list()
+for(i in 1:10) l2[[i]] = sim_pois_ar(n = 200, phi = 0.7, lam = 2)
+# set up cluster
+cl <- makeCluster(detectCores())
+# load our package for each node
+clusterEvalQ(cl, library(latentGaussCounts))
+# export data to each node
+clusterExport(cl, varlist = "l2")
+# Run LGC()
+system.time({
+out <- parLapply(cl, l2, function(x){
+                                      LGC(x,
+                                          count.family = "Poisson",
+                                          gauss.series = "AR", p=1,
+                                          estim.method = "particlesSIS")
+                                    })
+})
+# Stop the cluster
+stopCluster(cl)
+
+
+# ---- Linux or OS-X parallel -------------------------------------------------------
+library(parallel)
+# load data as list
+l2 <- list()
+for(i in 1:10) l2[[i]] = sim_pois_ar(n = 200, phi = 0.7, lam = 2)
+# Run LGC() function
+mclapply(l2, function(x){
+                          LGC(x,
+                              count.family = "Poisson",
+                              gauss.series = "AR", p=1,
+                              estim.method = "particlesSIS")
+                        }, mc.cores = detectCores())
+
