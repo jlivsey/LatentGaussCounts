76bce3505eb6913564ac52cb697d0a173a516d72
jlivsey <james.a.livsey@gmail.com>
2018-04-03 20:57:05 -0400

    setting up merge

--- R/g.R
+++ R/g.R
@@ -2,18 +2,20 @@
 # ---- g() from equation (23) on 1-31-18 ----
 
 
-#' g() function from latentGaussCounts paper. Equation (23) from draft 
+#' g() function from latentGaussCounts paper. Equation (23) from draft
 #' dated 1-28-18
 #'
 #' @param lam Poisson parameter
 #' @param k function input parameter
 #' @param polys list object containing the hermite polynomials. By default the
-#' orthopolynorm package is used and preloaded Polys.Rdata is loaded with the 
+#' orthopolynorm package is used and preloaded Polys.Rdata is loaded with the
 #' package
 #'
-#' @return function value 
+#' @return function value
 #'
-#' @examples
+
+
+
 g <- function(lam, k, polys=Polys){
   her <- as.function(polys[[k]]) # polys[[k]] = H_{k-1}
   N = which(round(ppois(1:100, lam), 7) == 1)[1]
@@ -21,4 +23,3 @@ g <- function(lam, k, polys=Polys){
     her(qnorm(ppois(0:N, lam, lower.tail = TRUE)))
   return(list(val=sum(terms)/sqrt(2*pi)/factorial(k), terms=terms))
 }
-# ----
