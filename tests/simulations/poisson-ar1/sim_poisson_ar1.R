f7a9741900a39662a9385ba389322b1b8ca122e2
J. Livsey <james.a.livsey@gmail.com>
2019-08-26 10:45:15 -0400

    added Pois-AR1 implied YW sim results

--- tests/simulations/poisson-ar1/sim_poisson_ar1.R
+++ tests/simulations/poisson-ar1/sim_poisson_ar1.R
@@ -1,7 +1,7 @@
 n.seq   = c(100, 200, 400)
 phi.seq = c(-.75, -.25, .25, .75)
 lam.seq = c(2, 5, 10)
-Nsim = 1000
+Nsim = 200
 total.iter = Nsim * length(n.seq) * length(phi.seq) * length(lam.seq)
 save.cols = c("estim.method", "n",
               "lam.true", "lam.est", "lam.se",
@@ -9,6 +9,8 @@ save.cols = c("estim.method", "n",
 simResults.poisAR1 = matrix(nrow = total.iter, ncol = length(save.cols))
 colnames(simResults.poisAR1) = save.cols
 
+
+# --- Add Gaussian Likelihood results to simResults matrix ----
 idx = 1
 pb <- txtProgressBar(min=2,max=total.iter,style=3)
 for(n.idx in 1:length(n.seq)){
@@ -38,7 +40,7 @@ close(pb)
 
 
 
-
+# ---- Add Particle filtering results to SimResults matrix ----
 # ---- Windows parallel -------------------------------------------------------
 library(parallel)
 # load data as list
@@ -116,4 +118,72 @@ mclapply(l2, function(x){
                               gauss.series = "AR", p=1,
                               estim.method = "particlesSIS")
                         }, mc.cores = detectCores())
+
+
+# ---- Add implied Yule-Walker estim results to SimResults matrix ----
+library(orthopolynom)
+polys <- hermite.he.polynomials(100) # In global env for g_coefs() function - BAD PROGRAMING
+source("~/Dropbox/jim/MVcopula/code/functions.R")
+n.coefs = 20
+
+# Current size of previous simulation results
+idx <- dim(simResults_poisson_ar1)[1] + 1
+
+# Set current results DF equal to old restuls
+simResults.poisAR1 <- simResults_poisson_ar1
+# Coerce 'estim.method' from factor to character
+simResults.poisAR1$estim.method <- as.character(simResults.poisAR1$estim.method)
+
+pb <- txtProgressBar(min = idx,
+                     max = total.iter + idx,
+                     style = 3)
+for(n.idx in 1:length(n.seq)){
+  for(phi.idx in 1:length(phi.seq)){
+    for(lam.idx in 1:length(lam.seq)){
+      for(dumb.variable in 1:Nsim){
+        n   = n.seq[n.idx]
+        phi = phi.seq[phi.idx]
+        lam = lam.seq[lam.idx]
+        x   = sim_pois_ar(n = n, phi = phi, lam = lam)
+
+        # Estimate Poisson mean param
+        lam.est = mean(x)
+
+        # Calculate Hermite coefficients
+        g.coefs = g_coefs(lam = lam.est, k = 1:n.coefs)
+
+        # Coefficients of f: gam.x --> gam.z
+        f.coefs = factorial(1:n.coefs) * g.coefs^2 / lam.est
+
+        # coefficients of f^-1: gam.z --> gam.x
+        finv.coefs = reversion(f.coefs)
+
+        # sample acf X
+        rho.x = acf(x, lag.max = 1, plot = FALSE, type = "correlation")$acf
+        rho.x = rho.x[-1]
+
+        # reversion to Z
+        rho.z = power_series(rho.x, finv.coefs)
+
+        # store param estimates matching previous format
+        param.est = c(lam.est, rho.z)
+
+        simResults.poisAR1[idx, "estim.method"] = "impliedYW"
+        simResults.poisAR1[idx, "n"] = n
+        simResults.poisAR1[idx, "lam.true"] = lam
+        simResults.poisAR1[idx, "lam.est"]  = param.est[1]
+        simResults.poisAR1[idx, "phi.true"] = phi
+        simResults.poisAR1[idx, "phi.est"]  = param.est[2]
+        idx = idx + 1
+        setTxtProgressBar(pb, idx)
+      }}}}
+close(pb)
+
+# Save results as .Rdata object
+# simResults.poisAR1 <- simResults_poisson_ar1
+# setwd("~/Desktop/LatentGaussCounts/tests/simulations/poisson-ar1")
+# save(simResults_poisson_ar1, file = "simResults_poisson_ar1.Rdata")
+
+
+
 
