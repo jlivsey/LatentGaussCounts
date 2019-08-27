a75223c3d4d706e85cf467d6824642fe4d3a4653
Vladas Pipiras <vladaspipiras@vladass-imac.wireless-1x.unc.edu>
2019-08-27 15:54:40 -0400

    testing

--- tests/test_liks_CRNs.R
+++ tests/test_liks_CRNs.R
@@ -42,17 +42,46 @@ optim.output <- optim(par = initial.param, fn = likSIS_AR1, setseed=TRUE,
 optim.output
 
 
-theta1.min = 0.01
-theta1.max = mean(x) + 30
 
-theta2.min = -.99
-theta2.max = .99
 
-optim.output2 <- optim(par = initial.param, fn = likSIS_AR1,
-                      data=x, setseed=TRUE, hessian=TRUE, method = "L-BFGS",
-                      lower = c(theta1.min, theta2.min),
-                      upper = c(theta1.max, theta2.max))
-optim.output2
+R = 100
+phi.est.all = rep(0,R)
+lam.est.all = rep(0,R)
+phi.std.all = rep(0,R)
+lam.std.all = rep(0,R)
+
+
+for (r in 1:R){
+
+  print(r)
+  set.seed(r)
+  x=sim_pois_ar(n = 400, phi = 0.7, lam = 2)
+  initial.param = c(count.initial(x), gauss.initial(x))
+
+  optim.output <- optim(par = initial.param, fn = likSIS_AR1, setseed=TRUE,
+                        data=x, hessian=TRUE, method = "BFGS")
+
+  phi.est.all[r]=optim.output$par[2]
+  lam.est.all[r]=optim.output$par[1]
+  phi.std.all[r]=sqrt(solve(optim.output$hessian))[2,2]
+  lam.std.all[r]=sqrt(solve(optim.output$hessian))[1,1]
+
+}
+
+
+phi.est.up = phi.est.all + 1.96*phi.std.all*sqrt(2) # sqrt(2) since -2*loglik is minimized
+phi.est.lo = phi.est.all - 1.96*phi.std.all*sqrt(2)
+plot(seq(1,R),phi.est.all,ylim=c(min(phi.est.lo),max(phi.est.up)))
+abline(h = .7, lty = 2, lwd = 1, col="red")
+segments(1:R, phi.est.lo, 1:R, phi.est.up, col = "blue", lty = 1)
+
+
+lam.est.up = lam.est.all + 1.96*lam.std.all*sqrt(2)
+lam.est.lo = lam.est.all - 1.96*lam.std.all*sqrt(2)
+plot(seq(1,R),lam.est.all,ylim=c(min(lam.est.lo),max(lam.est.up)))
+abline(h = 2, lty = 2, lwd = 1, col="red")
+segments(1:R, lam.est.lo, 1:R, lam.est.up, col = "blue", lty = 1)
+
 
 
 
